d8bca1e606638b5c1c1362c032f8120d008e0e0c
Daniel Terry <Daniel.N.Terry@hotmail.com>
2024-04-05 20:15:35 +1100

    Add Remove Book Functionality

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 const myLibrary = [];
 
+const myTable = {
+    rowSelected: NaN
+};
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -12,6 +16,10 @@ function Book(title, author, pages, read) {
         }
         return `${this.title} by ${this.author}, ${this.pages} pages, not read yet.`
     }
+
+    this.bookRead = function () {
+        this.read = this.read ? !this.read : this.read;
+    }
 }
 
 function addBookToLibrary(newBook) {
@@ -22,7 +30,6 @@ const updateTable = () => {
     const table = document.getElementsByClassName('table')[0];
     table.innerHTML = `
         <tr>
-            <th></th>
             <th>Name</th>
             <th>Author</th>
             <th>Pages</th>
@@ -31,17 +38,22 @@ const updateTable = () => {
     myLibrary.forEach((element) => {
         table.innerHTML += `
                         <tr>
-                            <td></td>
                             <td>${element.title}</td>
                             <td>${element.author}</td>
                             <td>${element.pages}</td>
                             <td>${element.read}</td>
                         </tr>`
     })
+
+    const tableRows = document.querySelectorAll("tr");
+    for (let i = 1; i < tableRows.length; i++) {
+        tableRows[i].addEventListener("click", () => {
+            myTable.rowSelected = i;
+        })
+    }
 }
 
 addEventListener('load', () => {
-
     updateTable();
 })
 
@@ -96,4 +108,15 @@ cancelBtn.addEventListener("click", () => {
     const inputs = document.getElementById("book-submission-form").elements;
     resetForm(inputs);
     hideForm();
+})
+
+const removeBookBtn = document.getElementById('removeBookBtn');
+
+removeBookBtn.addEventListener("click", () => {
+    if (!myTable.rowSelected) {
+        return;
+    }
+    myLibrary.splice(myTable.rowSelected - 1, 1);
+    myTable.rowSelected = NaN;
+    updateTable();
 })
